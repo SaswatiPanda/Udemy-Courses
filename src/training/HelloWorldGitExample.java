46ae40b3bd8ec902eb0d332d71178e24c3ccccc5
SaswatiPanda <39438021+SaswatiPanda@users.noreply.github.com>
2018-06-01 07:24:46 +0530

    Added the Hellow world Program

---
+++ src/training/HelloWorldGitExample.java
@@ -0,0 +1,9 @@
+package training;
+
+public class HelloWorldGitExample {
+	public static void main(String [] args)
+	{
+		System.out.println("Git should show this new class and the code changes that i have done");
+	}
+
+}
